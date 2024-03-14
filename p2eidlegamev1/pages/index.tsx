5447e57db26a0419f4fade835177381f6daa787f
SpreeGore <89117156+SpreeGore@users.noreply.github.com>
2024-03-13 21:33:55 -0600

    v1

--- p2eidlegamev1/pages/index.tsx
+++ p2eidlegamev1/pages/index.tsx
@@ -60,7 +60,7 @@ const Home: NextPage = () => {
         className={`${styles.mainButton} ${styles.spacerBottom}`}
         onClick={() => router.push("/play")}
       >
-        Play Game
+        Play The Game
       </button>
     </div>
   );
