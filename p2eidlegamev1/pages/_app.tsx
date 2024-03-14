dc6a0f7f7838c58d1188ae1246fe58e7b3d8a65d
SpreeGore <89117156+SpreeGore@users.noreply.github.com>
2024-03-13 21:49:45 -0600

    v1

--- p2eidlegamev1/pages/_app.tsx
+++ p2eidlegamev1/pages/_app.tsx
@@ -5,7 +5,7 @@ import "../styles/globals.css";
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "polygon";
+const activeChain = "mumbai";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
